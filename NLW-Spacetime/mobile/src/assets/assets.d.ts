ae9b9f8ec4f81c05ae46ae6007857a94fddd8923
Diogo_de_Paula <depaulatrabalhos@gmail.com>
2023-08-28 10:12:08 -0300

    comentário no mobile

--- NLW-Spacetime/mobile/src/assets/assets.d.ts
+++ NLW-Spacetime/mobile/src/assets/assets.d.ts
@@ -1,3 +1,5 @@
+// faz o typescript intender que todo arquivo que termine com  png pode ser importado
+// assim ele para de acusar erro nas importações de imagem
 declare module "*.png";
 
 declare module "*.svg" {
