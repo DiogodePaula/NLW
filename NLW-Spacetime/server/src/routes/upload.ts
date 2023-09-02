e2770cbe7e8543618df9c8f2ff63d3b6bc5a2be6
Diogo_de_Paula <depaulatrabalhos@gmail.com>
2023-09-01 23:30:10 -0300

    back aula 4

---
+++ NLW-Spacetime/server/src/routes/upload.ts
@@ -0,0 +1,46 @@
+import { FastifyInstance } from "fastify";
+// node: e para saber que esse crypto e um pacote interno do node e nao precisa ser instalado
+import { randomUUID } from "node:crypto";
+import { extname, resolve } from "node:path";
+import { createWriteStream } from "node:fs";
+import { pipeline } from "node:stream";
+import { promisify } from "node:util";
+
+const pump = promisify(pipeline);
+
+export async function uploadRoutes(app: FastifyInstance) {
+	app.post("/upload", async (req, reply) => {
+		const upload = await req.file({
+			limits: {
+				fileSize: 5_242_880, // 5mb, o underline e para facilitar a leitura nao afeta o numero
+			},
+		});
+
+		if (!upload) {
+			return reply.status(400).send();
+		}
+		// todo arquivo tem um mimetype, ele e uma categorização de tipos de arquivos
+		// para saber se ele e imagem ou video o mimetype dele deve começar com imagem ou video
+		const mimetypeRegex = /^(image|video)\/[a-zA-Z]+/;
+		const isValidFileFormat = mimetypeRegex.test(upload.mimetype);
+
+		if (!isValidFileFormat) {
+			return reply.status(400).send();
+		}
+
+		const fileId = randomUUID(); // caso o usuario faca upload de arquivos com nome igual vamos
+		//substituir para evitar nomes duplicados
+		const extension = extname(upload.filename);
+		const fileName = fileId.concat(extension);
+		const writeStream = createWriteStream(resolve(__dirname, "../../uploads/", fileName));
+
+		// Amazon S3, Google getComputedStyle, Cloudflare R2
+
+		await pump(upload.file, writeStream);
+
+		const fullUrl = req.protocol.concat("://").concat(req.hostname);
+		const fileUrl = new URL(`/uploads/${fileName}`, fullUrl).toString();
+
+		return { fileUrl };
+	});
+}
--- NLW-Spacetime/server/src/server.ts
+++ NLW-Spacetime/server/src/server.ts
@@ -2,18 +2,28 @@ import "dotenv/config";
 import fastify from "fastify";
 import jwt from "@fastify/jwt";
 import cors from "@fastify/cors";
+import multipart from "@fastify/multipart";
 
 import { memoriesRoutes } from "./routes/memories";
 import { authRoutes } from "./routes/auth";
+import { uploadRoutes } from "./routes/upload";
+import { resolve } from "path";
 
 const app = fastify();
-
+//Libs
 app.register(cors, {
 	origin: true, // todas URLs de front-end poderão acessar nosso back-end
 	// origin: ["http://localhost:3000", "https://APLICAÇÃO-PRODUÇÃO.com/memories"], uso em produção listar urls que poderão ter acesso
 });
 app.register(jwt, { secret: "spacetime" }); //o secret diferencia esse jwt de outros jwt
+app.register(multipart);
+app.register(require("@fastify/static"), {
+	root: resolve(__dirname, "../uploads"),
+	prefix: "/uploads",
+});
+//Rotas
 app.register(authRoutes);
+app.register(uploadRoutes);
 app.register(memoriesRoutes); // registra um arquivo de rotas separado
 
 app.listen({
