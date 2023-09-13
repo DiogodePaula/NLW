63b39ab817611518e9d6ff7bb8b8cee998577282
Diogo_de_Paula <depaulatrabalhos@gmail.com>
2023-09-12 21:32:19 -0300

    aula 1

--- NLW-Copa/server/src/routes/game.ts
+++ NLW-Copa/server/src/routes/game.ts
@@ -1,40 +1,40 @@
-import { FastifyInstance } from "fastify"
-import { prisma } from "../lib/prisma"
-import { z } from "zod"
-import { authenticate } from "../plugins/authenticate"
+import { FastifyInstance } from "fastify";
+import { prisma } from "../lib/prisma";
+import { z } from "zod";
+import { authenticate } from "../plugins/authenticate";
 
-export async function gameRoutes(fastify: FastifyInstance){
-    fastify.get('/pools/:id/games', { onRequest: [ authenticate ] }, async (req) => {
-        const getPoolParams = z.object({
-            id: z.string()
-        })
+export async function gameRoutes(fastify: FastifyInstance) {
+	fastify.get("/pools/:id/games", { onRequest: [authenticate] }, async (req) => {
+		const getPoolParams = z.object({
+			id: z.string(),
+		});
 
-        const { id } = getPoolParams.parse(req.params)
+		const { id } = getPoolParams.parse(req.params);
 
-        const games = await prisma.game.findMany({
-            orderBy: {
-                date: 'desc',
-            },
-            include: {
-                guesses: {
-                    where: {
-                        participant: {
-                            userId: req.user.sub,
-                            poolId: id,
-                        }
-                    }
-                }
-            }
-        })
+		const games = await prisma.game.findMany({
+			orderBy: {
+				date: "desc",
+			},
+			include: {
+				guesses: {
+					where: {
+						participant: {
+							userId: req.user.sub,
+							poolId: id,
+						},
+					},
+				},
+			},
+		});
 
-        return { 
-            games: games.map(game => {
-                return {
-                    ...game,
-                    guess: game.guesses.length > 0 ? game.guesses[0] : null,
-                    guesses: undefined
-                }
-            }) 
-        }
-    })
+		return {
+			games: games.map((game: any) => {
+				return {
+					...game,
+					guess: game.guesses.length > 0 ? game.guesses[0] : null,
+					guesses: undefined,
+				};
+			}),
+		};
+	});
 }
---
+++ NLW-IA/upload-ai-web/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [react()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+});
+// informa o vite da configuração de importação
