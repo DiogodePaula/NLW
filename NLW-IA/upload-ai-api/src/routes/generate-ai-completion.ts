c66f0ab4c7469f3035eab58ce74bbfe6f58c21a0
Diogo_de_Paula <depaulatrabalhos@gmail.com>
2023-09-15 16:21:19 -0300

    projeto concluido

--- NLW-IA/upload-ai-api/src/routes/generate-ai-completion.ts
+++ NLW-IA/upload-ai-api/src/routes/generate-ai-completion.ts
@@ -1,5 +1,6 @@
 import { FastifyInstance } from "fastify";
 import { z } from "zod";
+import { OpenAIStream, streamToResponse } from "ai";
 
 import { prisma } from "../lib/prisma";
 import { openAi } from "../lib/openai";
@@ -8,11 +9,11 @@ export async function generateAiCompletionRoute(app: FastifyInstance) {
 	app.post("/ai/generate", async (req, reply) => {
 		const paramsSchema = z.object({
 			videoId: z.string().uuid(),
-			template: z.string(),
+			prompt: z.string(),
 			temperature: z.number().min(0).max(1).default(0.5),
 		});
 
-		const { videoId, template, temperature } = paramsSchema.parse(req.body);
+		const { videoId, prompt, temperature } = paramsSchema.parse(req.body);
 
 		const video = await prisma.video.findUniqueOrThrow({
 			where: {
@@ -24,14 +25,22 @@ export async function generateAiCompletionRoute(app: FastifyInstance) {
 			return reply.status(400).send({ error: "Video transcription was not generated yet" });
 		}
 
-		const promptMessage = template.replace("{transcription}", video.transcription);
+		const promptMessage = prompt.replace("{transcription}", video.transcription);
 
 		const response = await openAi.chat.completions.create({
 			model: "gpt-3.5-turbo-16k", // aceita 16k de token, Tokenizer para calcular valores
 			temperature,
 			messages: [{ role: "user", content: promptMessage }],
+			stream: true,
 		});
 
-		return response;
+		const stream = OpenAIStream(response);
+
+		streamToResponse(stream, reply.raw, {
+			headers: {
+				"Access-Control-Allow-Origin": "*",
+				"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+			},
+		});
 	});
 }
--- NLW-IA/upload-ai-web/src/App.tsx
+++ NLW-IA/upload-ai-web/src/App.tsx
@@ -1,4 +1,6 @@
-import { FileVideo, Github, Upload, Wand2 } from "lucide-react";
+import { useState } from "react";
+import { Github, Wand2 } from "lucide-react";
+import { useCompletion } from "ai/react";
 
 import { Button } from "./components/ui/button";
 import { Separator } from "./components/ui/separator";
@@ -6,8 +8,24 @@ import { Textarea } from "./components/ui/textarea";
 import { Label } from "./components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./components/ui/select";
 import { Slider } from "./components/ui/slider";
+import { VideoInputForm } from "./components/video-input-form";
+import { PromptSelect } from "./components/prompt-select";
 
 export function App() {
+	const [temperature, setTemperature] = useState(0.5);
+	const [videoId, setVideoId] = useState<string | null>(null);
+
+	const { input, setInput, handleInputChange, handleSubmit, completion, isLoading } = useCompletion({
+		api: "http://localhost:3333/ai/generate",
+		body: {
+			videoId,
+			temperature,
+		},
+		headers: {
+			"Content-type": "application/json",
+		},
+	});
+
 	return (
 		<div className="min-h-screen flex flex-col flex-1">
 			<div className="px-6 py-3 flex items-center justify-between border-b">
@@ -28,50 +46,22 @@ export function App() {
 			<main className="flex-1 p-6 flex gap-6">
 				<div className="flex flex-col flex-1 gap-4">
 					<div className="grid grid-rows-2 gap-4 flex-1">
-						<Textarea className="resize-none p-4 leading-relaxed" placeholder="Inclua o prompt para a IA..." />
-						<Textarea className="resize-none p-4 leading-relaxed" placeholder="Resultado gerado pela IA..." readOnly />
+						<Textarea value={input} onChange={handleInputChange} className="resize-none p-4 leading-relaxed" placeholder="Inclua o prompt para a IA..." />
+						<Textarea value={completion} className="resize-none p-4 leading-relaxed" placeholder="Resultado gerado pela IA..." readOnly />
 					</div>
 					<p className="text-sm text-muted-foreground">
 						Lembre-se: você pode utilizar a variável <code className="text-violet-400">{"{transciption}"}</code> no seu prompt para adicionar o conteudo da transcrição do video selecionado
 					</p>
 				</div>
 				<aside className="w-80 space-y-6">
-					<form className="space-y-6 ">
-						<label
-							htmlFor="video"
-							className="border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5">
-							<FileVideo className="w-4 h-4" />
-							Selecione um video
-						</label>
-						<input type="file" id="video" accept="video/mp4" className="sr-only" />
-
-						<Separator />
-
-						<div className="space-y-2">
-							<Label htmlFor="transcription_prompt">Prompt de transcrição</Label>
-							<Textarea className="h-20 leading-relaxed resize-none" placeholder="Inclua palavras-chave mencionadas no video separadas por virgula (,)" />
-						</div>
-
-						<Button className="w-full">
-							Carregar video
-							<Upload className="h-4 w-4 ml-2" />
-						</Button>
-					</form>
+					<VideoInputForm onVideoUploaded={setVideoId} />
 
 					<Separator />
 
-					<form className="space-y-6">
+					<form onSubmit={handleSubmit} className="space-y-6">
 						<div className="space-y-2">
 							<Label>Prompt</Label>
-							<Select>
-								<SelectTrigger>
-									<SelectValue placeholder="Selecione um prompt..." />
-								</SelectTrigger>
-								<SelectContent>
-									<SelectItem value="title">Titulo do YouTube</SelectItem>
-									<SelectItem value="description">Descricao do YouTube</SelectItem>
-								</SelectContent>
-							</Select>
+							<PromptSelect onPromptSelected={setInput} />
 						</div>
 
 						<div className="space-y-2">
@@ -91,13 +81,13 @@ export function App() {
 
 						<div className="space-y-4">
 							<Label>Temperatura</Label>
-							<Slider min={0} max={1} step={0.1} />
+							<Slider min={0} max={1} step={0.1} value={[temperature]} onValueChange={(value) => setTemperature(value[0])} />
 							<span className="block text-xs text-muted-foreground italic leading-relaxed">Valores mais altos tendem a deixar o resultado mais criativo e com possíveis erros.</span>
 						</div>
 
 						<Separator />
 
-						<Button type="submit" className="w-full">
+						<Button disabled={isLoading} type="submit" className="w-full">
 							Executar
 							<Wand2 className="w-4 h-4 ml-2" />
 						</Button>
